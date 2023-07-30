19c4c83b89bd4b7a38c1a2b21acba61c99eb14d3
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-07-30 14:37:22 -0400

    Added final questions to Advanced folder

--- Challenges/Advanced/question3.js
+++ Challenges/Advanced/question3.js
@@ -1,1 +1,10 @@
-// TODO: FIND QUESTION
+// TODO: Find and return the median of the two arrays
+
+const nums1 = [1, 2, 3];
+const nums2 = [4, 5, 6];
+
+const findMedianSortedArrays = function (nums1, nums2) {
+  // Your code here
+};
+
+// Should return 3.5
