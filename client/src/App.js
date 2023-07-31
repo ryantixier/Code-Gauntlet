eb695f1e72102492b9a20a755818fda8289dc01c
unknown <john.gordos@outlook.com>
2023-07-31 12:36:15 -0600

    temporarily breaking App to test queries and mutations. Do not merge to main

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  InMemoryCache,
+  ApolloClient,
+  ApolloProvider,
+  useMutation,
+  useQuery,
+} from "@apollo/client";
+import {
+  QUERY_USER,
+  QUERY_CHALLENGES,
+  QUERY_CHALLENGE,
+  QUERY_SUBMISSIONS,
+} from "./databaseOperations/queries";
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 function App() {
+  const { loading, data } = useQuery(QUERY_CHALLENGES);
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ApolloProvider>
   );
 }
 
