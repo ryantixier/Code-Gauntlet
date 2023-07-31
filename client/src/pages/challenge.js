a5f61ba967ecf2234004097017af9461d47fec1e
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-07-31 14:29:28 -0600

    fixed erroring file paths in Challenge.js and in /ChallengeComps files

--- client/src/pages/challenge.js
+++ client/src/pages/challenge.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { NavBar } from "../components/NavComps/NavBar";
-import { ChallengeHeader } from "../components/ChallengeComps/ChallengeHeader";
-import { ChallengeBody } from "../components/ChallengeComps/ChallengeBody";
+import NavBar from "../components/NavComps/NavBar";
+import ChallengeHeader from "../components/ChallengeComps/ChallengeHeader";
+import ChallengeBody from "../components/ChallengeComps/ChallengeBody";
 
 export default function Challenge() {
   // styling/formatting here; ref wireframe
