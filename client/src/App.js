542084790eb3634c1a6dcac7f620a870221aae1d
corynko <ethankotel@gmail.com>
2023-07-31 10:59:30 -0600

    made pages visible via App.js, untangled currently existing react errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,165 @@
-import logo from './logo.svg';
-import './App.css';
+//react imports
+import * as React from "react";
 
-function App() {
+//nav router
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+//import app styles
+import "./App.css";
+
+//import page links
+// import Nav from "./components/NavComps/NavBar";
+import Home from "./pages/Home";
+import Challenge from "./pages/Challenge";
+import Profile from "./pages/Profile_2";
+import Leaderboard from "./pages/Leaderboard";
+
+//system-preference-set dark mode
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+export default function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  //ALERT: You may want to minimize the theme while working on this codebase
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+          //dark mode
+          primary: {
+            main: "#55628b",
+            light: "#7f90bd",
+            dark: "#39445e",
+          },
+          secondary: {
+            main: "#71ad87",
+          },
+          background: {
+            default: "#232323",
+            paper: "#242424",
+          },
+          text: {
+            primary: "#f5f5f5",
+            hint: "#111111",
+          },
+          error: {
+            main: "#ff8d70",
+          },
+          warning: {
+            main: "#ffcf56",
+          },
+          info: {
+            main: "#b3bad0",
+          },
+          success: {
+            main: "#5fad41",
+          },
+        },
+
+        //light mode
+        typography: {
+          fontSize: 16,
+          fontWeightLight: 200,
+          fontWeightRegular: 300,
+          fontWeightMedium: 400,
+          fontWeightBold: 600,
+          htmlFontSize: 17,
+          h1: {
+            fontFamily: "Montserrat",
+            fontSize: "6rem",
+            fontWeight: 200,
+            lineHeight: 1.3,
+            letterSpacing: "0.1em",
+          },
+          h2: {
+            fontFamily: "Raleway",
+            fontSize: "4.3rem",
+            fontWeight: 100,
+            lineHeight: 1.15,
+            letterSpacing: "0.03em",
+          },
+          h3: {
+            fontFamily: "Rubik",
+          },
+          h4: {
+            fontFamily: "Raleway",
+          },
+          subtitle1: {
+            fontFamily: "Montserrat",
+          },
+          subtitle2: {
+            fontFamily: "Rubik",
+            fontSize: "1.1rem",
+          },
+          body1: {
+            fontFamily: "Rubik",
+            fontSize: "1.4rem",
+          },
+          body2: {
+            fontFamily: "Montserrat",
+            fontWeight: 500,
+            fontSize: "1.3rem",
+          },
+          button: {
+            fontFamily: "Rubik",
+            fontWeight: 600,
+            fontSize: "1.4rem",
+          },
+          caption: {
+            fontFamily: "Raleway",
+            fontSize: "1rem",
+          },
+          overline: {
+            fontFamily: "Raleway",
+            fontSize: "1.1rem",
+          },
+          h5: {
+            fontFamily: "Raleway",
+            fontSize: "1.8rem",
+          },
+          h6: {
+            fontSize: "1.5rem",
+          },
+        },
+        overrides: {
+          MuiButton: {
+            root: {
+              background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+              border: 0,
+              borderRadius: 3,
+              boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+              color: "white",
+              height: 48,
+              padding: "0 30px",
+            },
+          },
+        },
+        props: {
+          MuiAppBar: {
+            color: "default",
+          },
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="container">
+          {/* <Nav /> */}
+          <Routes>
+            <Route path="/challenge" element={<Challenge />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/*" element={<Home />} />
+          </Routes>
+        </div>
+      </ThemeProvider>
+    </Router>
   );
 }
-
-export default App;
