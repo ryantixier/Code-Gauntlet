71026e86e6bf48ad666a3b0a8435932a422cc809
unknown <john.gordos@outlook.com>
2023-08-01 09:38:01 -0600

    do not merge, testing mutations

--- client/src/Test.js
+++ client/src/Test.js
@@ -1,17 +1,42 @@
 import { useMutation, useQuery } from "@apollo/client";
 import {
-  QUERY_USER,
-  QUERY_CHALLENGES,
-  QUERY_CHALLENGE,
-  QUERY_SUBMISSIONS,
+  QUERY_USER, //good
+  QUERY_CHALLENGES, //good
+  QUERY_CHALLENGE, //good
+  QUERY_SUBMISSIONS, //good
 } from "./databaseOperations/queries";
+import {
+  ADD_USER,
+  LOGIN_USER,
+  ADD_SUBMISSION,
+  removeSubmission,
+  addVote,
+  removeVote,
+} from "./databaseOperations/mutations";
 function Test() {
-  const { loading, data } = useQuery(QUERY_USER, {
-    userId: "64c81328f1b90d5d29cbc253",
-  });
-  console.log(data);
-
-  return <>{data}</>;
+  const { loading: loadingChallenge, data: challengeData } = useQuery(
+    QUERY_CHALLENGE,
+    {
+      variables: {
+        challengeId: "64c81329f1b90d5d29cbc25d",
+      },
+    }
+  );
+  const { loading: loadingSubmission, data: submissionData } = useQuery(
+    QUERY_SUBMISSIONS,
+    {
+      variables: {
+        challengeId: "64c81329f1b90d5d29cbc25d",
+      },
+    }
+  );
+  if (loadingChallenge || loadingSubmission) {
+    return <>Loadin!</>;
+  } else {
+    console.log(challengeData);
+    console.log(submissionData);
+    return <>Check the Logs!</>;
+  }
 }
 
 export default Test;
