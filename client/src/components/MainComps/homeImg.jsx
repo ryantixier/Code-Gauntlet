86f0a7beb74b215028cde4cb9c00bffe4fc9eafd
corynko <ethankotel@gmail.com>
2023-08-02 12:10:25 -0600

    lots of homepage changes

--- client/src/components/MainComps/homeImg.jsx
+++ client/src/components/MainComps/homeImg.jsx
@@ -1,7 +1,12 @@
 // import { animate, delay, motion } from "framer-motion";
 
 function HomeImg() {
-  return <div className="center coverImg homeCover" />;
+  return (
+    <>
+      <div className="center coverImg screenCover" />
+      <div className="center coverImg frameCover" />
+    </>
+  );
 }
 
 export default HomeImg;
--- client/src/components/NavComps/NavLogo.jsx
+++ client/src/components/NavComps/NavLogo.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import logo from "../../assets/images/code_gauntlet_horiz_text_on_trans.png";
+import { Link } from "react-router-dom";
 
 export default function NavLogo() {
   return (
     <div>
-      <img
-        className="navImg"
-        height="200px"
-        width="200px"
-        src={logo}
-        alt="Code Gauntlet Logo"
-      />
+      <Link className="navLink" to="/">
+        <img
+          className="navImg"
+          height="200px"
+          width="200px"
+          src={logo}
+          alt="Code Gauntlet Logo"
+        />
+      </Link>
     </div>
   );
 }
